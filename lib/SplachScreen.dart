60d1808cbf49e8e1724d6fad59186fc695ea44f4
Sarahuucc <minjih968@gmail.com>
2025-01-17 08:20:33 +0300

    Update SplachScreen.dart

--- lib/SplachScreen.dart
+++ lib/SplachScreen.dart
@@ -1,6 +1,38 @@
-import 'package:bookbloom/ShapesClasses/SplachShape.dart';
-import 'package:flutter/material.dart';
+// import 'package:bookbloom/ShapesClasses/SplachShape.dart';
+// import 'package:flutter/material.dart';
+//
+//
+// class Splachscreen extends StatelessWidget {
+//   const Splachscreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Column(
+//           children: [
+//             Transform.translate(
+//               offset: const Offset(-30, 50), //المسافات
+//               child: CustomPaint(
+//                 // رسمة splash
+//
+//                 size: const Size(354, 401),
+//                 painter: RPSCustomPainter(),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//
+//             );
+//   }
+// }
+//
+//
 
+import 'package:flutter/material.dart';
+import 'package:bookbloom/ShapesClasses/SplachShape.dart';
+import 'package:bookbloom/LoginScreen.dart';
 
 class Splachscreen extends StatelessWidget {
   const Splachscreen({super.key});
@@ -10,21 +42,92 @@ class Splachscreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Transform.translate(
-              offset: const Offset(-30, 50), //المسافات
+              offset: const Offset(-40, -120),
               child: CustomPaint(
-                // رسمة splash
-
-                size: const Size(354, 401),
+                size: const Size(400, 80),
                 painter: RPSCustomPainter(),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            Image.asset(
+              'assets/images/bookbloom.png',
+              width: 250,
+              height: 200,
+            ),
+      Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20),
+           child:  RichText(
+
+              text: TextSpan(
+
+                text: 'Welcome to ',
+                style: const TextStyle(
+
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'BookBloom',
+                    style: const TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xffD4ACA5),
+                    ),
+                  ),
+                  const TextSpan(
+                    text: ' where Stories Flourish!',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+      ),
+            const SizedBox(height: 30),
+      Padding(
+        padding: const EdgeInsets.only(left: 22.0, right: 22),
+            child:  Text(
+              'Explor a World of Free Reading and Writing Where Every Story is Just a Page Away',
+              style: TextStyle(
+
+                fontSize: 16,
+              ),
+            ),
+      ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => Loginscreen()),
+                );
+              },
+              child: const Text("Let's Go",
+                  style: TextStyle(  fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,),
+              ),
+              style: ElevatedButton.styleFrom(
+
+                fixedSize: Size(200, 50),
+                backgroundColor:Color(0xffD4ACA5),
               ),
             ),
           ],
         ),
       ),
-      
-      
     );
   }
 }
+
+
